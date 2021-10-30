47ac1b5f2a4fdc8ba37dd951251d7bf8d56c4b78
PavelFilonov <qqq.qqq48@yandex.ru>
2021-10-30 22:16:39 +0300

    Implemented repository of contracts

--- src/main/java/com/ru/nc/edu/filonov/repositories/ContractRepository.java
+++ src/main/java/com/ru/nc/edu/filonov/repositories/ContractRepository.java
@@ -2,32 +2,39 @@ package com.ru.nc.edu.filonov.repositories;
 
 import com.ru.nc.edu.filonov.entities.contracts.Contract;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
 
 public class ContractRepository implements Repository<Contract> {
 
-    private final List<Contract> contracts = new ArrayList<>();
-
+    private Set<Contract> contracts = new HashSet<>();
 
     @Override
     public boolean add(Contract item) {
-        return false;
+        return contracts.add(item);
     }
 
     @Override
     public boolean addAll(Collection<Contract> items) {
-        return false;
+        return contracts.addAll(items);
     }
 
     @Override
     public boolean removeById(Long id) {
+        Optional<Contract> item = get(id);
+        if (item.isPresent()) {
+            contracts.remove(item);
+            return true;
+        }
         return false;
     }
 
     @Override
-    public Contract get(Long id) {
-        return null;
+    public Optional<Contract> get(Long id) {
+        return contracts.stream()
+                .filter(item -> item.getId().equals(id))
+                .findFirst();
     }
 }
--- src/main/java/com/ru/nc/edu/filonov/repositories/Repository.java
+++ src/main/java/com/ru/nc/edu/filonov/repositories/Repository.java
@@ -1,11 +1,12 @@
 package com.ru.nc.edu.filonov.repositories;
 
 import java.util.Collection;
+import java.util.Optional;
 
 public interface Repository<T> {
 
     boolean add(T item);
     boolean addAll(Collection<T> items);
     boolean removeById(Long id);
-    T get(Long id);
+    Optional<T> get(Long id);
 }
